4081fb15f047fe51eca4e3ad7f0c179f9e6e85f1
Saadat Ali <Sadaat.khan@salik.ae>
2020-02-12 17:04:55 +0400

    code checkin

--- TranscoreDashboard/src/com/transcore/controller/UserAccountController.java
+++ TranscoreDashboard/src/com/transcore/controller/UserAccountController.java
@@ -25,7 +25,6 @@ public class UserAccountController extends HttpServlet {
         super();
         
     }
-
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
